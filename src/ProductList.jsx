54ca0aad656e876dcea3ce0e7ea449e756855ee3
udemeabasi <udemeabasi6@gmail.com>
2024-10-27 11:08:07 -0400

    update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -287,7 +287,8 @@ function ProductList() {
                                 <div className="product-card" key={plantIndex}>
                                  <img className="product-image" src={plant.image} alt={plant.name} />
                                     <div className="product-title">{plant.name}</div>
-                                     {/*Similarly like the above plant.name show other details like description and cost*/}
+                                     <div className="product-cost">{plant.cost}</div>
+                                     <div className="product-description">{plant.description}</div>
                                      <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                                     </div>
                                     ))}
